c6871754b1a3939bb12765ffe0bbf5a0717212b1
andrey12v <andrey12v@gmail.com>
2022-02-06 18:02:01 -0600

    explanation commit for useEffect

--- src/hooks/use-counter.js
+++ src/hooks/use-counter.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react';
 const useCounter = (forwards = true) => {
   const [counter, setCounter] = useState(0);
 
+  //we added firwards as input parameter and we also added it as dependency
+  //this useEffect will rerun when forwards dependency changes
   useEffect(() => {
     const interval = setInterval(() => {
       if (forwards) {
